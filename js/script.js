97fb4f131613a761c3a22a2abc8a5d8a68203d12
SimoneSpirito <simospiri01@gmail.com>
2021-11-11 20:40:33 +0100

    template literal

--- js/script.js
+++ js/script.js
@@ -22,3 +22,30 @@ const text = [
     'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Et temporibus voluptatum suscipit tempore aliquid deleniti aut veniam inventore eligendi ex ad ullam,',
     'Et temporibus voluptatum suscipit tempore aliquid deleniti aut veniam inventore eligendi ex ad ullam,',
 ]
+
+let content = '';
+let mignaturs = '';
+
+for (let i = 0; i < items.length; i++) {
+    content += `<div class="item">
+                    <img src="${items[i]}" alt="${title[i]}">
+                    <div class="item-text">
+                            <h3>${title[i]}</h3>
+                            <p>${text[i]}</p>
+                    </div>
+                </div>`;
+    
+    mignaturs += `<div class="prev"><i class="fas fa-chevron-up"></i></div>
+                  <div class="next"><i class="fas fa-chevron-down"></i></div>
+                  
+                  <div class="mignatur">
+                     <img src="${items[i]}" alt="${title[i]}">
+                  </div>`
+}
+
+const itemContainer = document.querySelector('.item-container');
+itemContainer.innerHTML = content;
+
+const sliderMin = document.querySelector('.sl-min');
+sliderMin.innerHTML = mignaturs;
+
